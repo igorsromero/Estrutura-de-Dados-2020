292fef3a4185084103f139445813b0f8df247a49
Igor Solfa Romero <igorsolfaromero@gmail.com>
2020-11-25 00:26:22 -0300

    Notação Polonesa Reversa

---
+++ Projetos/Notação Polonesa Reversa/npr.py
@@ -0,0 +1,36 @@
+
+
+def adicionarpilha(pilha, valor):
+    pilha.append(valor)
+
+
+def removerpilha(pilha, qtd):
+    for i in range(qtd):
+        pilha.pop()
+
+
+NPR = []
+try:
+    while True:
+        var = input()
+        adicionarpilha(NPR, var)
+        pos = len(NPR)-1
+        if var == '+':
+            var = float(NPR[pos-1]) + float(NPR[pos-2])
+            removerpilha(NPR, 3)
+            adicionarpilha(NPR, var)
+        elif var == '*':
+            var = float(NPR[pos-1]) * float(NPR[pos-2])
+            removerpilha(NPR, 3)
+            adicionarpilha(NPR, var)
+        elif var == '/':
+            var = float(NPR[pos-1]) / float(NPR[pos-2])
+            removerpilha(NPR, 3)
+            adicionarpilha(NPR, var)
+        elif var == '-':
+            aux = float(NPR[pos-1]) - float(NPR[pos-2])
+            removerpilha(NPR, 3)
+            adicionarpilha(NPR, var)
+        print(var)
+except ValueError:
+    print("Não foi possível realizar a operação.")
