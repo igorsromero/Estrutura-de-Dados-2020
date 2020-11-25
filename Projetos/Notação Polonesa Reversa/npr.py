5f0e00ddcf6212aee18986d07f1777c52ae357f1
Igor Solfa Romero <igorsolfaromero@gmail.com>
2020-11-25 19:06:07 -0300

    Projeto Notação Polonesa Reversa

--- Projetos/Notação Polonesa Reversa/npr.py
+++ Projetos/Notação Polonesa Reversa/npr.py
@@ -1,36 +1,39 @@
 def adicionarpilha(pilha, valor):
-    pilha.append(valor)
+    return pilha.append(valor)
 
 
 def removerpilha(pilha, qtd):
     for i in range(qtd):
         pilha.pop()
+    return True
 
 
 NPR = []
+
+print('Pressione ENTER após inserir um número ou a operação.')
 try:
     while True:
         var = input()
         adicionarpilha(NPR, var)
         pos = len(NPR)-1
         if var == '+':
-            var = float(NPR[pos-1]) + float(NPR[pos-2])
+            var = float(NPR[pos-2]) + float(NPR[pos-1])
             removerpilha(NPR, 3)
             adicionarpilha(NPR, var)
         elif var == '*':
-            var = float(NPR[pos-1]) * float(NPR[pos-2])
+            var = float(NPR[pos-2]) * float(NPR[pos-1])
             removerpilha(NPR, 3)
             adicionarpilha(NPR, var)
         elif var == '/':
             try:
-                var = float(NPR[pos-1]) / float(NPR[pos-2])
+                var = float(NPR[pos-2]) / float(NPR[pos-1])
                 removerpilha(NPR, 3)
                 adicionarpilha(NPR, var)
             except ZeroDivisionError:
                 print("Impossível dividir por 0.")
                 removerpilha(NPR, 1)
         elif var == '-':
-            var = float(NPR[pos-1]) - float(NPR[pos-2])
+            var = float(NPR[pos-2]) - float(NPR[pos-1])
             removerpilha(NPR, 3)
             adicionarpilha(NPR, var)
         print(var)
